9bf2c87a9d02e5e586b36cd2a9f18f9927e7fa68
Alex Moreno <alex.moreno.costa@gmail.com>
2018-07-02 10:27:47 -0300

    Fiz a tela de detalhe do pedido

--- src/AppBundle/Entity/Sale.php
+++ src/AppBundle/Entity/Sale.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Sale
@@ -185,7 +186,11 @@ class Sale
 
     public function calculateSubTotal()
     {
-        return array_reduce($this->items, function ($total, SaleItem $saleItem) {
+        $items = is_array($this->items) ?
+            $this->items :
+            $this->items->toArray();
+
+        return array_reduce($items, function ($total, SaleItem $saleItem) {
             $total += $saleItem->calculateTotal();
             return $total;
         }, 0);
